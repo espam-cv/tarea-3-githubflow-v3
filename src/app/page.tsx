6530b9d14cc2f2a555d2ecbd49d8415024735f2a
luisRiofrio <lriofrio915@gmail.com>
2023-11-14 22:09:05 -0500

    se aplica estilos css al idex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
@@ -26,33 +26,29 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <div className="calculator">
-          <div className="display">
+        <div className={styles.calculator}>
+          <div className={styles.display}>
             <input type="text" value={input} readOnly />
-            <div className="result">{result}</div>
+            <div className={styles.result}>{result}</div>
           </div>
-          <div className="buttons">
-            <button onClick={() => handleButtonClick("7")}>7</button>
-            <button onClick={() => handleButtonClick("8")}>8</button>
-            <button onClick={() => handleButtonClick("9")}>9</button>
-            <button onClick={() => handleButtonClick("/")}>/</button>
-
-            <button onClick={() => handleButtonClick("4")}>4</button>
-            <button onClick={() => handleButtonClick("5")}>5</button>
-            <button onClick={() => handleButtonClick("6")}>6</button>
-            <button onClick={() => handleButtonClick("*")}>*</button>
-
+          <div className={styles.buttons}>
             <button onClick={() => handleButtonClick("1")}>1</button>
             <button onClick={() => handleButtonClick("2")}>2</button>
             <button onClick={() => handleButtonClick("3")}>3</button>
-            <button onClick={() => handleButtonClick("-")}>-</button>
-
+            <button onClick={() => handleButtonClick("4")}>4</button>
+            <button onClick={() => handleButtonClick("5")}>5</button>
+            <button onClick={() => handleButtonClick("6")}>6</button>
+            <button onClick={() => handleButtonClick("7")}>7</button>
+            <button onClick={() => handleButtonClick("8")}>8</button>
+            <button onClick={() => handleButtonClick("9")}>9</button>
             <button onClick={() => handleButtonClick("0")}>0</button>
+            <button onClick={() => handleButtonClick("+")}>+</button>
+            <button onClick={() => handleButtonClick("-")}>-</button>
+            <button onClick={() => handleButtonClick("*")}>*</button>
+            <button onClick={() => handleButtonClick("/")}>/</button>
             <button onClick={() => handleButtonClick(".")}>.</button>
             <button onClick={handleCalculate}>=</button>
-            <button onClick={() => handleButtonClick("+")}>+</button>
-
-            <button onClick={handleClear} className="clear">
+            <button onClick={handleClear} className={styles.clear}>
               C
             </button>
           </div>
